a6270fc152bd146b980db2fd770b1a9d27a3627b
iSoul95 <lmeasures95@gmail.com>
2023-06-27 12:49:34 +0100

    api features 'completed' - need testing and validation

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
-import {express} from 'express';
+import express, { Request, Response } from 'express';
 import { MongoClient } from 'mongodb';
 
 const connectionString = 'mongodb://localhost:27017/mydb';
@@ -6,23 +6,46 @@ const connectionString = 'mongodb://localhost:27017/mydb';
 const app = express();
 app.use(express.json());
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log("Server Listening on PORT:", PORT);
-})
+const PORT = 3001;
 
-const userSearchHandler = (req, res) => {
-    
+const userSearchHandler = async (req: Request, res: Response) => {
+    try{
+        const client = await MongoClient.connect(connectionString);
+        const db = client.db();
+        const usersCollection = db.collection('users');
+
+        const str = req.params.search
+
+        const regex = new RegExp(req.params.search, "i");
+
+        const dbQuery = { name: regex };
+        const results = await usersCollection
+                                .find(dbQuery)
+                                .toArray();
+
+        client.close();
+        
+        console.log("Results, get!: ");
+        results.forEach(user => {
+            console.log(user);
+        });
+
+        res.status(200).send(results);
+    }   
+    catch (e) {
+        console.error("Error searching for users", e);
+        res.status(500);
+    }
 }
 
-const postUserHandler = async (req, res) => {
+const postUserHandler = async (req: Request, res: Response) => {
 
     // Validate requestBody
 
     try { 
         const client = await MongoClient.connect(connectionString);
         const db = client.db();
-        const userCollection = db.collection('users');
+        const usersCollection = db.collection('users');
 
         const newUser = {
             name: req.body.name,
@@ -31,13 +54,16 @@ const postUserHandler = async (req, res) => {
             role: req.body.role,
         }
 
-        const response = await userCollection.insertOne(newUser);
+        const response = await usersCollection.insertOne(newUser);
         console.log('Entry Added, UserId: ', response.insertedId);
 
         client.close();
+
+        res.status(204);
     }
     catch(e){
         console.error("Error writing user to database: ", e)
+        res.status(500);
     }
 }
 
@@ -45,10 +71,17 @@ const postUserHandler = async (req, res) => {
 app.get("/userSearch", userSearchHandler);
 app.post("/user", postUserHandler);
 
+app.get("/", (req: Request, res: Response) => {
+    console.log("Hello World!");
+});
 
 app.get("/status", (req, res) => {
     const status = {
         "Status": "Running"
     };
-    res.status(204).send(status);
+    res.send(status);
+})
+
+app.listen(PORT, () => {
+    console.log("Server Listening on PORT:", PORT);
 })
