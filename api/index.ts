f135f60c2c9bc248c52c8eeefdbc2cd48426c6ca
iSoul95 <lmeasures95@gmail.com>
2023-06-27 11:56:05 +0100

    added create user functionality- no validation

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,7 @@
 import {express} from 'express';
+import { MongoClient } from 'mongodb';
+
+const connectionString = 'mongodb://localhost:27017/mydb';
 
 const app = express();
 app.use(express.json());
@@ -9,12 +12,35 @@ app.listen(PORT, () => {
 })
 
 const userSearchHandler = (req, res) => {
-    return
+    
 }
 
-const postUserHandler = (req, res) => {
-    return
+const postUserHandler = async (req, res) => {
+
+    // Validate requestBody
+
+    try { 
+        const client = await MongoClient.connect(connectionString);
+        const db = client.db();
+        const userCollection = db.collection('users');
+
+        const newUser = {
+            name: req.body.name,
+            email: req.body.email,
+            phone: req.body.phone,
+            role: req.body.role,
+        }
+
+        const response = await userCollection.insertOne(newUser);
+        console.log('Entry Added, UserId: ', response.insertedId);
+
+        client.close();
+    }
+    catch(e){
+        console.error("Error writing user to database: ", e)
+    }
 }
+
 
 app.get("/userSearch", userSearchHandler);
 app.post("/user", postUserHandler);
